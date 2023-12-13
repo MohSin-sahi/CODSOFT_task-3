f66a48c6549ecf177370524377b201c7f4e52e13
Mohsin <mohsinhp918@gmail.com>
2023-12-14 02:22:45 +0500

    script file updated and remove js from index file

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let result = document.querySelector(".display");
+"use strict";
 
-function display(number) {
-  result.value += number;
-}
-function calculate() {
-  let final = result.value;
-  let final_result = eval(final);
-  result.value = final_result;
-}
-function clr() {
-  result.value = "";
-}
-function del() {
-  result.value = result.value.slice(0, -1);
-}
+const buttons = document.querySelectorAll(".btn");
+let input = document.querySelector(".display");
+let string = "";
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.innerText === "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerText === "C") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerText === "Del") {
+      string = string.slice(0, -1);
+      input.value = string;
+    } else {
+      string += e.target.innerText;
+      input.value = string;
+    }
+  });
+});
